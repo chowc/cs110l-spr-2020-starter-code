4236269f0974ad02379e20c971ffcc443efed732
chowc <1398296215@qq.com>
2022-02-20 16:11:35 +0800

    week6

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,14 +1,62 @@
 use crossbeam_channel;
 use std::{thread, time};
+use std::sync::mpsc::{channel, sync_channel};
+use std::sync::Arc;
 
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
-    F: FnOnce(T) -> U + Send + Copy + 'static,
+    F: FnOnce(T) -> U + Send + Copy + 'static + Sync,
     T: Send + 'static,
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    let mut senders = vec![];
+    let mut receivers = vec![];
+    for _ in 0..num_threads {
+        let (s, r) = channel();
+        senders.push(s);
+        receivers.push(r);
+    }
+    for (idx, e) in input_vec.into_iter().enumerate() {
+        let sender = senders[idx%num_threads].clone();
+        sender.send((idx, e));
+    }
+    drop(senders);
+
+    let mut handlers = vec![];
+    let (result_sender, result_receiver) = channel();
+    let f = Arc::new(f);
+    for receiver in receivers {
+        let f = f.clone();
+        let result_sender = result_sender.clone();
+        let handler = thread::spawn(move || {
+            loop {
+                match receiver.recv() {
+                    Ok((idx, e)) => {
+                        let result = f(e);
+                        result_sender.send((idx, result));
+                    },
+                    _ => break,
+                }
+            }
+        });
+        handlers.push(handler);
+    }
+    for handler in handlers {
+        handler.join();
+    }
+    drop(result_sender);
+    for _ in 0..output_vec.capacity() {
+        output_vec.push(U::default());
+    }
+    for _ in 0..output_vec.capacity() {
+        match result_receiver.recv() {
+            Ok((idx, r)) => {
+                output_vec[idx] = r;
+            },
+            _ => break,
+        }
+    }
     output_vec
 }
 
